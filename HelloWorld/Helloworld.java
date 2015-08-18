ef4975b9d4a9727bb9ad5b11183b84f9fdf62c59
soccerlexi13 <soccerlexi13@gmail.com>
2015-08-18 09:21:30 -0700

    First Program Done!
    Hello World!

---
+++ HelloWorld/Helloworld.java
@@ -0,0 +1,14 @@
+
+/**
+ * My first Java program (with an improper class name, it should be HelloWorld), print out Hello World
+ * 
+ * @author Alexis Scheerer
+ * @version 8-18-2015
+ */
+public class Helloworld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Alexis!");
+    }
+}
