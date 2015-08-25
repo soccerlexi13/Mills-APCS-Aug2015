451a2fdfaf1ed5477b65dc7cd5d14b05ad520195
soccerlexi13 <soccerlexi13@gmail.com>
2015-08-25 08:54:51 -0700

    :+1:

---
+++ Java Challenge/StarFigures.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class StarFigures here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StarFigures
+{
+   public static void main(String[] args)
+   {
+       PrintTwoFiveAsterisks();
+       PrintFiveAsterisks();
+       PrintTwoFiveAsterisks();
+       PrintFiveAsterisks();
+       PrintTwoFiveAsterisks(); 
+       System.out.println("  *\n  *\n  *");
+       PrintTwoFiveAsterisks();
+       PrintFiveAsterisks();
+       
+   }
+   public static void PrintFiveAsterisks()
+   {
+       System.out.println(" * *");
+       System.out.println("  *");
+       System.out.println(" * *");
+       System.out.println(""); 
+   }
+    public static void PrintTwoFiveAsterisks()
+   {
+       System.out.println("*****");
+       System.out.println("*****");
+   }
+}
